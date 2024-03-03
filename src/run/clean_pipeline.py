ec61edfc992ed9ff584603cc92071d3a32e5612b
arunpshankar <arunprshankar@gmail.com>
2024-03-03 15:30:27 -0500

    wip

--- src/run/clean_pipeline.py
+++ src/run/clean_pipeline.py
@@ -6,29 +6,64 @@ from src.db.delete import delete_table
 from src.config.setup import logger
 
 
-# clean table
-delete_table()
+def delete_search_apps(prefix: str) -> None:
+    """
+    Deletes search apps that have a display name starting with the specified prefix.
 
-# clean data stores and search apps 
-try:
-    engines = list_apps()
-    for engine in engines:
-        display_name = engine.get('displayName', '')
-        if display_name.startswith('moodys_site_search'):
-            logger.info(f"Deleting app: {display_name}")
-            name = engine.get('name', '')
-            delete_app(name)
+    Args:
+    - prefix (str): The prefix to filter search apps by.
 
-    data_stores = list_data_stores()
-    for data_store in data_stores:
-        display_name = data_store.get('displayName', '')
-        if display_name.startswith('moodys_site_search'):
-            logger.info(f"Deleting data store: {display_name}")
-            data_store_name = data_store.get('name', '')
-            delete_data_store(data_store_name)
-except Exception as e:
-    logger.error(f"Error during execution: {e}")
+    Returns:
+    - None
+    """
+    try:
+        engines = list_apps()
+        for engine in engines:
+            display_name = engine.get('displayName', '')
+            if display_name.startswith(prefix):
+                logger.info(f"Deleting app: {display_name}")
+                name = engine.get('name', '')
+                delete_app(name)
+    except Exception as e:
+        logger.error(f"Error deleting search apps: {e}")
 
-# clean gcs bucket 
+def delete_data_stores(prefix: str) -> None:
+    """
+    Deletes data stores that have a display name starting with the specified prefix.
 
+    Args:
+    - prefix (str): The prefix to filter data stores by.
 
+    Returns:
+    - None
+    """
+    try:
+        data_stores = list_data_stores()
+        for data_store in data_stores:
+            display_name = data_store.get('displayName', '')
+            if display_name.startswith(prefix):
+                logger.info(f"Deleting data store: {display_name}")
+                data_store_name = data_store.get('name', '')
+                delete_data_store(data_store_name)
+    except Exception as e:
+        logger.error(f"Error deleting data stores: {e}")
+
+def main():
+    """
+    Main function to orchestrate the cleanup of tables, search apps, and data stores.
+
+    Returns:
+    - None
+    """
+    # Clean Cloud SQL table 
+    delete_table()
+
+    # Clean search apps and data stores with a specific prefix
+    prefix = "moodys_site_search"
+    delete_search_apps(prefix)
+    delete_data_stores(prefix)
+
+    # Clean GCS bucket 
+
+if __name__ == "__main__":
+    main()
