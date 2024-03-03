fb23d05ecdb480fe70da145aaa3894baa33359e5
arunpshankar <arunprshankar@gmail.com>
2024-03-03 11:22:15 -0500

    wip

---
+++ src/run/query_pipeline.py
@@ -0,0 +1,13 @@
+from src.config.logging import logger 
+from src.config.setup import * 
+
+
+if __name__ == '__main__':
+    query = 'stanford university Master of Science in Computer Science'
+    # LLM to extract the university name 
+    # resolve using embeding 
+    # match against SQL db 
+    # get the batch_id 
+    # construct the API call with the targeted query 
+    # make the API call 
+    # display the results 
